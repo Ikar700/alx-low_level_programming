acf0c52643510fa4877230055a5ff44f84fe77b6
Olubadejo Cornelius <cornex111@gmail.com>
2022-11-07 19:14:50 -0800

    initialized repo and task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string
+ * followed by a new line
+ *
+ * @s: string to be copied
+ */
+void _puts_recursion(char *s)
+{
+	if *s
+	{
+		_putchar(*s);
+		_puts_recurion(*s +1);
+	}
+
+	else 
+		_putchar('\n');
+}
