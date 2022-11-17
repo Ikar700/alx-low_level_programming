78b97d28546cd072eac0f395e7e27801196e748d
Olubadejo Cornelius <cornex111@gmail.com>
2022-11-17 06:38:20 -0800

    tyask 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument passed into it
+ * followed by a new line
+ * @args: the number of arguments passed into the program
+ * @argv: an array of pointers to the program
+ *
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
