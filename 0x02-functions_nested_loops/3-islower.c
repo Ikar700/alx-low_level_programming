ba8951905e1933c7428b783173938612d0225571
Ikar700 <Cornex111@gmail.com>
2023-10-31 15:57:02 -0700

    removing shii

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if c is lowercase 
+ * returns 0 if not
+ *
+ * Return: 1 if c is lowercase
+ * Return: 0 if not 
+ */
+
+int _islower(int c)
+{
+
+}
