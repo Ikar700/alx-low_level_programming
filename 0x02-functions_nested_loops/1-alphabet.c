c311de998cf9e4f95ec55fabc8b3e7dfc410bec7
Ikar700 <Cornex111@gmail.com>
2023-10-31 10:12:01 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, followed by a new line
+ *
+ * Return: 0 Always success
+ */
+void print_alphabet(void)
+{
+	_putchar('abcdefghijklmnopqrstuvwxyz');
+	_putchar('\n');
+}
